ac3f26699c6fbef9a7d9d54430c8f79715e88a70
BenjaminLivingstone <benjaminlivingstone@gmail.com>
2021-08-02 21:47:15 -0400

    First Commit-OK

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
+
+
+def random_word(request):
+    if 'count' in request.session:
+        temp=request.session['count']+1
+        request.session['count']=temp
+    else:
+        request.session['count']=1
+    context = {
+        "title": "Random Word",
+        "word": get_random_string(length=14),
+    }
+    return render(request,'app/index.html', context)
+
+def reset(request):
+    request.session['count']=0
+    return redirect("/")
